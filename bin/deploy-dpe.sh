709b1bc67e09b2d502c27422a82b0bb1c91759ac
ron-barrett <ronald.e.barrett@centene.com>
2019-07-29 09:19:23 -0400

    test commit

---
+++ bin/deploy-dpe.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+HOME_FOLDER="~dpe"
+
+while getopts "dpj:" opt; do
+    case $opt in
+        p )
+            passkey=$OPTARG
+            ;;
+
+        j )
+            HOME_FOLDER="~"
+            ;;
+
+        d )
+            dry_run=" --dryrun"
+            ;;
+
+        \? )
+            usage
+            ;;
+
+        : ) echo "ERROR: Invalid option: $OPTARG requires an argument" 1>&2
+            usage
+            ;;
+    esac
+done
+
+#Verify Input: passkey
+if [ "$passkey" = "" ]
+   then
+     echo "ERROR: Must provide a passkey"
+     usage
+fi
